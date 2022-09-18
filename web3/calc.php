da53ed4282f292095da89ae1babe7660f3c98d63
unknown <tolentinojanglenard5@gmail.com>
2022-09-19 02:26:26 +0800

    done web3

--- web3/calc.php
+++ web3/calc.php
@@ -2,13 +2,32 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+
+        $operations = $_POST['calc'];
+        $val1 = $_POST['val1'];
+        $val2 = $_POST['val2'];
+
         # test if input is_numeric
-        if(1){
+        if(is_numeric($val1) && is_numeric($val2)){
+
             # test if calc is not null
-            if(1){
+            if(!empty($val1) && !empty($val2)){
+                
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($operations){
+                    case "add":
+                        $result = $val1+$val2;
+                        break;
+                    case "mul":
+                        $result = $val1*$val2;
+                        break;
+                    case "sub":
+                        $result = $val1-$val2;
+                        break;
+                    case "div":
+                        $result = $val1/$val2;
+                        break;
+
                 }
                 echo "Calculation result: $result";
             }
