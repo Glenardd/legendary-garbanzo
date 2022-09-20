e480beb45328e388cfff8d9839364027d2a0a1d3
Arnel1001 <202080475@psu.palawan.edu.ph>
2022-09-20 16:32:35 +0800

    done web5

--- web5/index.php
+++ web5/index.php
@@ -3,18 +3,20 @@
  //session_unset();
  // use of isset function to check if session is variable is already or not
  // $num_visit incremented each time the page is visited during session
- // $_SESSION variables that can be accesed during session.
- 		
-
-		 if(isset( $_SESSION['$num_visits']) ){
-		  		 $_SESSION['$num_visits'] +=1;
-		   }else {
-		      $_SESSION['$num_visits'] = 1;
-		      echo " <h2> Wow </h2> Thank you for visiting the page a lot. ";
-		   		
-		   }
+ // $_SESSION variables that can be accessed during session.
 ?>
 
 			<h1>Hello</h1>
 			<p>You have visited this page <?= $_SESSION['$num_visits'] ?> times</p>
 			<p><a href="?reset=true">Reset</a></p>
+
+<?php
+$_SESSION['$num_visits']+=1;
+if (isset($_SESSION['$num_visits'])){
+	if ($_SESSION['$num_visits']==6){
+	echo "Thank you for visiting the page a lot.";}
+	if ($_SESSION['$num_visits']==11){
+	header("Location: congratulations.php", TRUE, 301);
+	exit();}
+}
+?>
