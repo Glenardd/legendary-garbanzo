ad0ddd3134ad1ecda0a56a32a167667387dc822e
roseaban131 <filletchicken387@gmail.com>
2022-09-20 11:02:26 +0800

    session added

--- web5/index.php
+++ web5/index.php
@@ -1,6 +1,6 @@
 <?php
  session_start();
- //session_unset
+ //session_unset();
  // use of isset function to check if session is variable is already or not
  // $num_visit incremented each time the page is visited during session
  // $_SESSION variables that can be accesed during session.
@@ -9,11 +9,12 @@
 		 if(isset( $_SESSION['$num_visits']) ){
 		  		 $_SESSION['$num_visits'] +=1;
 		   }else {
-		      $_SESSION['$num_visits'] == 5;
+		      $_SESSION['$num_visits'] = 0;
 		      echo " <h2> Wow </h2> Thank you for visiting the page a lot. ";
 		   		
 		   }
 ?>
+
 			<h1>Hello</h1>
 			<p>You have visited this page <?= $_SESSION['$num_visits'] ?> times</p>
 			<p><a href="?reset=true">Reset</a></p>
