9cc6f79dc2c593c118d4fb634195bfe9815672ec
roseaban131 <filletchicken387@gmail.com>
2022-09-20 12:03:16 +0800

    done web4

--- web4/index.php
+++ web4/index.php
@@ -1,27 +1,3 @@
-<?php
-    # to avoid undefined errors from the post method
-    error_reporting(0);
-
-    # used post method to get the data's form private
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    #cookie variables
-    $cookie_auth = "auth";
-    $cookie_value = "OK";
-    $cookie_user = "username";
-    $cookie_pass = "password";
-
-    #cookie set with time limit of 20 seconds
-    #the loggedin.php will read this
-    if(!empty($username) && !empty($password)){
-        setcookie($cookie_user, $username, time()+20);
-        setcookie($cookie_pass, $password, time()+20);
-        setcookie($cookie_auth, $cookie_value, time()+20);
-        header("location: loggedin.php");
-    }
-
-?>
 <?php 
 session_start();
 ?>
@@ -45,7 +21,7 @@ session_start();
     echo "<br>";
     echo "Where are these session variables stored?"; 
     echo "<br>";
-    $_SESSION['stor']=" --Session variables that are only stored on the server.";
+    $_SESSION['stor']="--Session variables stored and that are only saved on the server.";
 
     ?>
 </body>
